97932eed8c5e104d9f3a9f2ba0f8fde35371cfd8
GonzaSalda <gonza.salda00@gmail.com>
2024-05-23 17:29:55 -0300

    add answer correct logic

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import Results from "./Results";
+
 const Question = ({
   filteredQuestion,
   setIndexQuestion,
@@ -5,52 +8,117 @@ const Question = ({
   questionsFiltered,
   setActiveQuiz,
 }) => {
+
+  const [score, setScore] = useState(0)
+  const [answersRandom, setAnswersRandom] = useState([]);
+  const [selectAnswerIndex, setSelectAnswerIndex] = useState(null);
+  const[answered, setAnswered] = useState(false)
+  const [activeResults, setActiveResults] = useState(false)
+
+  useEffect(() => {
+    const answer = [
+      ...filteredQuestion.incorrect_answers,
+      filteredQuestion.correct_answer,
+    ];
+
+    setAnswersRandom(answer.sort(() => Math.random() - 0.5));
+  }, [filteredQuestion]);
+
+  const checkAnswer = (answerText, index) => {
+    if(answerText === filteredQuestion.correct_answer){
+        setScore(score + 1)
+    }
+    setSelectAnswerIndex(index)
+    setAnswered(true)
+  }
+
   const onNextQuestion = () => {
     setIndexQuestion(indexQuestion + 1);
+    setSelectAnswerIndex(null)
+    setAnswered(false)
   };
+
+  const onReset = () => {
+    setScore(0);
+    setActiveQuiz(false);
+    setIndexQuestion(0);
+  }
 
   return (
     <>
-      <div className="flex flex-col justify-between shadow-md shadow-slate-300 w-[600px] h-[600px] p-10 rounded-lg">
-        <div className="flex justify-between">
-          <span className="text-xl font-bold">
-            {/* Num preg actual y numero de preguntas total */}
-            {indexQuestion + 1} / {questionsFiltered.length}
-          </span>
-          <div>
-            <span className="font-semibold">Dificultad:</span>
-            <span className="font-bold">
-              {/* La dificultad de la pregunta */}
-            </span>
-          </div>
-        </div>
+        {
+            activeResults ? (
 
-        <button
-          onClick={() => setActiveQuiz(false)}
-          className="border px-5 py-2 rounded-lg font-bold transition-all hover:bg-yellow-500 hover:text-gray-900"
-        >
-          Reiniciar
-        </button>
-        <div>
-          <h1 className="font-bold">Titulo de la pregunta</h1>
-        </div>
+                <Results questionsFiltered = {questionsFiltered} score={score} onReset={onReset}/>
 
-        {/* Respuestas */}
-        <div className="grid grid-cols-2 gap-5">
-          {/* Mapeamos una respuesta correcta y respuesta incorrecta */}
-          <button className="border-p-5 rounded-lg flex justify-center items-center hover:scale-105">
-            <p className="font-medium text-center text-sm">Alguna respuesta</p>
-          </button>
-        </div>
-
-        {/* Condicional para mostrar sig preg o el de finalizar */}
-        <button
-          onClick={onNextQuestion}
-          className="border-2 border-yellow-600 text-yellow-600 rounded-md px-5 py-2 hover:bg-yellow-600 hover:text-black font-medium"
-        >
-          Siguiente Pregunta
-        </button>
-      </div>
+            ) : (
+                <div className="flex flex-col justify-between shadow-md shadow-slate-300 w-[600px] h-[600px] p-10 rounded-lg">
+                <div className="flex justify-between">
+                  <span className="text-xl font-bold">
+                    {/* Num preg actual y numero de preguntas total */}
+                    {indexQuestion + 1} / {questionsFiltered.length}
+                  </span>
+                  <div>
+                    <span className="font-semibold">Dificultad:</span>
+                    <span className="font-bold">
+                      {/* La dificultad de la pregunta */}
+                        {filteredQuestion.difficulty}
+                    </span>
+                  </div>
+                </div>
+        
+        
+                <button
+                  onClick={() => setActiveQuiz(false)}
+                  className="border px-5 py-2 rounded-lg font-bold transition-all hover:bg-yellow-500 hover:text-gray-900"
+                >
+                  Reiniciar
+                </button>
+        
+                {/* Pregunta */}
+                <div>
+                  <h1 className="font-bold">{filteredQuestion.question}</h1>
+                </div>
+        
+                {/* Respuestas */}
+                <div className="grid grid-cols-2 gap-5">
+                  {/* Mapeamos una respuesta correcta y respuesta incorrecta */}
+                  {answersRandom.map((answer, index) => (
+                    <button
+                      key={index}
+                      className="border-p-5 rounded-lg flex justify-center items-center hover:scale-105"
+                      onClick={() => checkAnswer(answer, index)}
+                   >
+                      <p className="font-medium text-center text-sm">{answer}</p>
+                    </button>
+                  ))}
+                </div>
+        
+                {/* Condicional para mostrar sig preg o el de finalizar */}
+                 {
+                    indexQuestion + 1 === questionsFiltered.length ? (
+        
+                        <button
+                        onClick={() => {
+                            setAnswered(false)
+                            setActiveResults(true)
+                        }}
+                        className="border-2 border-yellow-600 text-yellow-600 rounded-md px-5 py-2 hover:bg-yellow-600 hover:text-black font-medium"
+                      >
+                        Finalizar
+                      </button>
+                    ) : (
+                        <button
+                        onClick={onNextQuestion}
+                        className="border-2 border-yellow-600 text-yellow-600 rounded-md px-5 py-2 hover:bg-yellow-600 hover:text-black font-medium"
+                      >
+                        Siguiente Pregunta
+                      </button>
+                    )
+                 }
+              </div>
+            )
+        }
     </>
   );
 };
--- src/pages/ClassPage.jsx
+++ src/pages/ClassPage.jsx
@@ -3,58 +3,63 @@ import { useParams } from "react-router-dom";
 import { imgs, questions } from "../data";
 import Question from "../components/Question";
 
-
 const ClassPage = () => {
-
-  const shuffleArray = array =>{
-   const newArray = array.sort(() => Math.random() - 0.5);
-   return newArray.slice(0, 5);
+  const shuffleArray = (array) => {
+    const newArray = array.sort(() => Math.random() - 0.5);
+    return newArray.slice(0, 5);
   };
 
-  const {classl2} = useParams();
-  const algo = imgs.filter(img => img === `/src/assets/${classl2.toLowerCase()}.jpg`)
+  const { classl2 } = useParams();
+  const [imgClass] = imgs.filter(
+    (img) => img === `/src/assets/${classl2.toLowerCase()}.jpg`
+  );
 
-  const [questionsFiltered, SetQuestionsFiltered] = useState(questions.filter(question => question.category === classl2))
-  const [indexQuestion, setIndexQuestion] = useState(0)
-  const [activeQuiz, setActiveQuiz] = useState(false)
+  const [questionsFiltered, SetQuestionsFiltered] = useState(
+    questions.filter((question) => question.category === classl2)
+  );
+  const [indexQuestion, setIndexQuestion] = useState(0);
+  const [activeQuiz, setActiveQuiz] = useState(false);
+  
   useEffect(() => {
     const newQuestions = shuffleArray(questionsFiltered);
     SetQuestionsFiltered(newQuestions);
-  }, [])
+  }, []);
 
-  
   return (
     <>
-          <div className="container flex flex-col items-center justify-center gap-10"
-          style={{height: 'calc(100vh - 5rem)'}}>
-            {activeQuiz ? (
-                 <Question 
-                 filteredQuestion={questionsFiltered[indexQuestion]}
-                 setIndexQuestion= {setIndexQuestion}
-                 indexQuestion= {indexQuestion}
-                 questionsFiltered={questionsFiltered}
-                 setActiveQuiz = {setActiveQuiz}
-                 />
-            ): (
-              <>
-                 <div className="flex flex-col gap-5">
-                    <h1 className="text-3xl text-teal-900 text-center font-bold">
-                      {classl2}
-                    </h1>
+      <div
+        className="flex flex-col items-center justify-center gap-10"
+        style={{ height: "calc(100vh - 5rem)" }}
+      >
+        {activeQuiz ? (
+          <Question
+            filteredQuestion={questionsFiltered[indexQuestion]}
+            setIndexQuestion={setIndexQuestion}
+            indexQuestion={indexQuestion}
+            questionsFiltered={questionsFiltered}
+            setActiveQuiz={setActiveQuiz}
+          />
+        ) : (
+          <>
+            <div className="flex flex-col  gap-5">
+              <h1 className="text-3xl text-teal-900 text-center font-bold">
+                {classl2}
+              </h1>
 
-                    <div className="flex justify-center items-center">
-                      <img src={algo } alt={classl2} className="w-72"/>
-                    </div>
-                  </div>              
+              <div className="flex justify-center items-center">
+                <img src={imgClass} alt={classl2} className="w-72" />
+              </div>
+            </div>
 
-                  <button className="text-white bg-gray-900 py-2 rounded-lg font-bold px-5 transition-all hover:bg-yellow-500 hover:text-gray-900"
-                  onClick={() => setActiveQuiz(true)}>
-                      Empezar Quiz
-                  </button>
-              </>
-            )}
-           
-          </div>
+            <button
+              className="text-white bg-gray-900 py-2 rounded-lg font-bold px-5 transition-all hover:bg-yellow-500 hover:text-gray-900"
+              onClick={() => setActiveQuiz(true)}
+            >
+              Empezar Quiz
+            </button>
+          </>
+        )}
+      </div>
     </>
   );
 };
